3d6006d24a2874be7c681c2ed98b876b91e7b216
Adrian Egli <adrian.egli@sbb.ch>
2026-06-29 23:11:07 +0200

    Add layout sandbox component

--- frontend/src/app/features/layout/index.ts
+++ frontend/src/app/features/layout/index.ts
@@ -1,3 +1,4 @@
 export * from './components/panel-shell/panel-shell.component';
 export * from './components/panel-plugin-host/panel-plugin-host.component';
 export * from './components/layout-renderer/layout-renderer.component';
+export * from './pages/layout-sandbox/layout-sandbox.component';
